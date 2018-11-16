6c03b540a4f32d0fa3cb6da1f57fadb5a7eca15e
MrMantou <albert_nchu@163.com>
2018-11-16 17:54:09 +0800

    添加应用服务

---
+++ EventCloud.MVC.Core/aspnet-core/src/EventCloud.Application/Events/Dtos/EventRegisterOutput.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventCloud.Events.Dtos
+{
+    public class EventRegisterOutput
+    {
+        public int RegistrationId { get; set; }
+    }
+}
---
+++ EventCloud.MVC.Core/aspnet-core/src/EventCloud.Application/Events/EventAppService.cs
@@ -0,0 +1,76 @@
+﻿using Abp.Application.Services.Dto;
+using Abp.Authorization;
+using Abp.Domain.Repositories;
+using EventCloud.Authorization.Users;
+using EventCloud.Events.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventCloud.Events
+{
+    [AbpAuthorize]
+    public class EventAppService : EventCloudAppServiceBase, IEventAppService
+    {
+        private readonly IEventManager eventManager;
+        private readonly IRepository<Event, Guid> eventRepository;
+
+        public EventAppService(IEventManager eventManager, IRepository<Event, Guid> eventRepository)
+        {
+            this.eventManager = eventManager;
+            this.eventRepository = eventRepository;
+        }
+
+
+
+
+
+        public async Task CreateAsync(CreateEventInput input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<EventDetailOutput> GetDetailAsync(EntityDto<Guid> input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ListResultDto<EventListDto>> GetListAsync(GetEventListInput input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task CancelAsync(EntityDto<Guid> input)
+        {
+            var @event = await eventManager.GetAsync(input.Id);
+
+        }
+
+        public async Task<EventRegisterOutput> RegisterAsync(EntityDto<Guid> input)
+        {
+            var registration = await RegisterAndSaveAsync(
+                await eventManager.GetAsync(input.Id),
+                await GetCurrentUserAsync());
+
+            return new EventRegisterOutput
+            {
+                RegistrationId = registration.Id
+            };
+        }
+
+        public async Task CancelRegistrationAsync(EntityDto<Guid> input)
+        {
+            await eventManager.CancelRegistrationAsync(
+                await eventManager.GetAsync(input.Id),
+                await GetCurrentUserAsync());
+        }
+
+        private async Task<EventRegistration> RegisterAndSaveAsync(Event @event, User user)
+        {
+            var registration = await eventManager.RegisterAsync(@event, user);
+            await CurrentUnitOfWork.SaveChangesAsync();
+            return registration;
+        }
+    }
+}
